f22e8cc6a3806ce7165e175d187111b937004f67
Jaemnie <bensygaming@gmail.com>
2024-03-25 18:48:52 +0900

    1차
    1차 구현

--- event/event.js
+++ event/event.js
@@ -1,10 +1,46 @@
 $(document).ready(function() {
-    // Accordion 기본적으로 숨김 처리
-    $("#accordion").hide();
+    // 초기에 #accordion과 #shop 숨김
+    $("#accordion, #shop, #feedback, #selectable").hide();
 
-    // icon1 클릭 이벤트 처리
+
+    $("#accordion").accordion({
+        collapsible: true,
+        active: false,
+        heightStyle: "fill",
+    });
+
+    $("#accordion-resizer").resizable({
+        minHeight: 140,
+        minWidth: 200,
+        resize: function() {
+            $("#accordion").accordion("refresh");
+        }
+    });
+
+    // #icon1 클릭 이벤트 처리
     $("#icon1").click(function() {
-        // Accordion 표시/숨김 토글
+        // #shop을 숨기고 #accordion 표시/숨김 토글
+        $("#shop, #feedback, #selectable").hide();
         $("#accordion").toggle();
     });
+
+    // #icon2 클릭 이벤트 처리
+    $("#icon2").click(function() {
+        // #accordion을 숨기고 #shop 표시/숨김 토글
+        $("#accordion").hide();
+        $("#shop, #feedback, #selectable").toggle();
+    });
+    
 });
+
+$( function() {
+    $( "#selectable" ).selectable({
+      stop: function() {
+        var result = $( "#select-result" ).empty();
+        $( ".ui-selected", this ).each(function() {
+          var index = $( "#selectable li" ).index( this );
+          result.append( " #" + ( index + 1 ) );
+        });
+      }
+    });
+  } );
