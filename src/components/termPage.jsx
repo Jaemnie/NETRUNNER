813e09b8260349112b63b139465be5e5fd12d2f0
jsh9298 <58097733+jsh9298@users.noreply.github.com>
2024-04-03 15:46:44 +0900

    히히 메인뚝딱시작

---
+++ src/components/termPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+const TermPage=()=> {
+    return (
+            <>
+                <div>filesystem</div>
+                <div id="term" style={{width: "100%",height: "100%"}}></div>
+            </>
+    );
+}
+export default TermPage;
