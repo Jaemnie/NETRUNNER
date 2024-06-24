ab501cf427d7abe05b8f51dd580d8874c94be559
Jaemnie <bensygaming@gmail.com>
2024-06-24 14:05:44 +0900

    커밋

--- src/components/termPage.jsx
+++ src/components/termPage.jsx
@@ -1,29 +1,33 @@
 import { Terminal } from "@xterm/xterm";
 import "../../node_modules/@xterm/xterm/css/xterm.css";
-import React, { useRef, useEffect, useState } from "react";
+import React, { useRef, useEffect } from "react"; // useState 제거
 import { Termi } from "./termsocket";
-import  TerminalInteraction  from './TerminalInteraction';
-import {SocketResult} from "./socket";
+import TerminalInteraction from './TerminalInteraction';
+import { SocketResult } from "./socket";
+import './terminal.css'; // 커스텀 CSS 가져오기
 
 const TermPage = () => {
   const termRef = useRef(null);
   const socketRoomId = Math.floor(100000 + Math.random() * 900000).toString();
   const socket = new SocketResult();
   socket.joinRoom(socketRoomId);
+
   useEffect(() => {
     if (termRef.current) {
       const term = new Terminal();
-      Termi(term, termRef.current,socket.getRoomId()); 
+      Termi(term, termRef.current, socket.getRoomId());
       TerminalInteraction.setTerminal(term);
       return () => {
-        term.dispose(); // Terminal 인스턴스 정리
+        term.dispose(); // 터미널 인스턴스 정리
       };
     }
-  },[]);
-  return (<>
-    {/* DirectoryViewer 컴포넌트로 파일 시스템 내용을 렌더링 */}
-    <div ref={termRef} />
-  </>
+  }, [socket]); // socket 의존성 추가
+
+  return (
+    <>
+      <div ref={termRef} className="terminal-container" /> {/* 커스텀 CSS 클래스 적용 */}
+    </>
   );
 }
+
 export default TermPage;
